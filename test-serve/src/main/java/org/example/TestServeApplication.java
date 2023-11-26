b6ba033bf5f6c97992b5343abeeeadf70aa46e8d
userA <geniusssbg@gmail.com>
2023-11-26 18:14:01 +0800

    Initial commit

---
+++ test-serve/src/main/java/org/example/TestServeApplication.java
@@ -0,0 +1,10 @@
+package org.example;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestServeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TestServeApplication.class, args);
+    }
+}
